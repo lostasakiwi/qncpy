32187d62356f62e7d1679b0b2fa5522435a613ad
zdata <zdata@tuxrules.org>
2021-10-17 13:30:26 -0400

    Updated for http to show anything that is not 200

--- qncpy.py
+++ qncpy.py
@@ -6,7 +6,7 @@ import re
 import requests
 import socket
 import sys
-from time import perf_counter
+from time import perf_counter, time
 requests.packages.urllib3.disable_warnings()
 
 
@@ -88,9 +88,9 @@ def http_port_check(url, ports):
         http_request = f'{url}:{port}'
         try:
             http_response = requests.get(http_request, timeout=3, verify=False)
-            if http_response.status_code in range(100, 500):
+            if http_response.status_code == 200:
                 dict_insert(http_up, url, port.strip(' ') + f' :{http_response.status_code}')
-            elif http_response.status_code in range(500, 600):
+            else:
                 dict_insert(http_down, url, port.strip(' ') + f' :{http_response.status_code}')
         except Exception as http_err:
             dict_insert(http_down, url, port.strip(' ') + f' :No Response')
@@ -175,18 +175,18 @@ def write_report(start_time):
         stop_time = perf_counter()
         f.write(f"{time_stamp()}  ")
         f.write(f"Total run time: {stop_time - start_time:.2f}s")
-        f.write(f"\nhttp hosts NOT responding or 500+ response\n{'-' * 50}\n")
+        f.write(f"\nhttp hosts NOT responding or not 200 \n{'-' * 50}\n")
         dict_writer(http_down, f)
         f.write(f"\ntcp host ports NOT responding\n{'-' * 50}\n")
         dict_writer(tcp_down, f)
-        f.write(f"\nhttp hosts reporting 100-499 [port :response code]\n{'-' * 50}\n")
+        f.write(f"\nhttp hosts reporting 200 [port :response code]\n{'-' * 50}\n")
         dict_writer(http_up, f)
         f.write(f"\ntcp hosts responding OK\n{'-' * 50}\n")
         dict_writer(tcp_up, f)
         f.write(f"\nErrors detected in config_file\n{'-' * 50}\n")
         dict_writer(errors, f)
         print("\nReport written to", today_write_filename_gen())
-        print("\nversion = 0.8013") 
+        print("\nversion = 0.8014") 
 
 
 def print_report(start_time):
@@ -194,11 +194,11 @@ def print_report(start_time):
         stop_time = perf_counter()
         print(f"\n{time_stamp()}  ")
         print(f"Total run time: {stop_time - start_time:.2f}s")
-        print(f"\nhttp hosts NOT responding or 500+ response\n{'-' * 50}\n")
+        print(f"\nhttp hosts NOT responding or not 200 \n{'-' * 50}\n")
         dict_printer(http_down, f)
         print(f"\ntcp host ports NOT responding\n{'-' * 50}\n")
         dict_printer(tcp_down, f)
-        print(f"\nhttp hosts reporting 100-499 [port :response code]\n{'-' * 50}\n")
+        print(f"\nhttp hosts reporting 200 [port :response code]\n{'-' * 50}\n")
         dict_printer(http_up, f)
         print(f"\ntcp hosts responding OK\n{'-' * 50}\n")
         dict_printer(tcp_up, f)
@@ -212,10 +212,11 @@ def main():
     config_data = read_config_file()       # read in config data for processing
     check_parse_config(config_data)        # validate qncpy.conf is able to be parsed successfully
     run_config_data(config_data)           # process http & tcp checks
-    print_report(start_time)               # print report to screen
     write_report(start_time)               # write out to report file
+    print_report(start_time)               # print report to screen
 
 
 if __name__ == "__main__":
     errors, tcp_up, tcp_down, http_up, http_down = {}, {}, {}, {}, {}
     main()
+
